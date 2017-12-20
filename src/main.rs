3651df17453874693af1258c69d279d430a4eac7
stuart nelson <stuartnelson3@gmail.com>
2017-12-20 21:53:25 +0100

    "works"
    There's a delay between running the code and the
    update taking place on the screen. I don't know
    what triggers it, or how to force it. Calling
    flush doesn't seem to work.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![feature(libc)]
+extern crate libc;
+extern crate xcb;
+
+use xcb::ffi::xproto::xcb_change_property;
+use libc::c_void;
+
 fn main() {
-    println!("Hello, world!");
+    let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
+    let setup = conn.get_setup();
+    let root = setup.roots().nth(screen_num as usize).unwrap().root();
+    let message = "tc-73db9 | 99% | test".to_owned();
+    let data = message.as_ptr() as *const c_void;
+
+    unsafe {
+        xcb_change_property(
+            conn.get_raw_conn(),
+            xcb::ffi::xproto::XCB_PROP_MODE_REPLACE as u8,
+            root,
+            xcb::ffi::xproto::XCB_ATOM_WM_NAME,
+            xcb::ffi::xproto::XCB_ATOM_STRING,
+            8 as u8,
+            message.len() as u32,
+            data,
+        );
+    }
+    conn.flush();
 }
