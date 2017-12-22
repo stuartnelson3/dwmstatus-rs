7219427cbfe92b628e4dda3d045ca92f4b34974c
stuart nelson <stuartnelson3@gmail.com>
2017-12-22 14:09:31 +0100

    Wait in loop for update

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,28 @@ fn main() {
     let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
     let setup = conn.get_setup();
     let root = setup.roots().nth(screen_num as usize).unwrap().root();
-    let message = "tc-73db9 | 99% | test".to_owned();
-    let data = message.as_ptr() as *const c_void;
+    let one_sec = std::time::Duration::new(1, 0);
+    let mut percent = 100;
 
-    unsafe {
-        xcb_change_property(
-            conn.get_raw_conn(),
-            xcb::ffi::xproto::XCB_PROP_MODE_REPLACE as u8,
-            root,
-            xcb::ffi::xproto::XCB_ATOM_WM_NAME,
-            xcb::ffi::xproto::XCB_ATOM_STRING,
-            8 as u8,
-            message.len() as u32,
-            data,
-        );
+    loop {
+        let message = format!("tc-73db9 | {}% | test", percent);
+        let data = message.as_ptr() as *const c_void;
+
+        unsafe {
+            xcb_change_property(
+                conn.get_raw_conn(),
+                xcb::ffi::xproto::XCB_PROP_MODE_REPLACE as u8,
+                root,
+                xcb::ffi::xproto::XCB_ATOM_WM_NAME,
+                xcb::ffi::xproto::XCB_ATOM_STRING,
+                8 as u8,
+                message.len() as u32,
+                data,
+            );
+        }
+        conn.flush();
+
+        percent -= 1;
+        std::thread::sleep(one_sec);
     }
-    conn.flush();
 }
